ab9875cc06ef52e18963216593a2240ddc687bfa
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2024-09-05 16:16:35 -0300

    feat: add routes

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,10 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('/todos', 'TodosController@getTodos');
+$router->get('/todos/{todo}', 'TodosController@getTodo');
+$router->post('/todos', 'TodosController@postTodo');
+$router->put('/todos/{todo}', 'TodosController@putTodo');
+$router->patch('/todos/{todo}/status/{status}', 'TodosController@patchTodoStatus');
+$router->delete('/todos/{todo}', 'TodosController@deleteTodo');
